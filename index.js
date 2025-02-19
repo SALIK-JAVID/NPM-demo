549a225d8449b5c145cbf1ee840b59fe61bfbebf
SALIK JAVID <148994667+SALIK-JAVID@users.noreply.github.com>
2025-02-19 20:59:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// var generateName = require("sillyname");  // common js method of native modules 
+
+// import generateName from "sillyname";   // ESM ecmascript modules 
+// var sillyName = generateName();
+
+// console.log(`My random name is ${sillyName}`);
+
+
+// import superheroes from "superheroes";
+
+// const name = superheroes.random();
+
+// console.log(`My favroite superhero is ${name}`);
+
+
+import superheroes from 'superheroes';
+
+// const name = superheroes;
+
+// console.log(`my name is ${name}`);
+
+const name = superheroes.random();
+console.log(`I am ${name}!`);
+
+
